3cfc62b517f4b973baf5a1418a0761176f40ce5c
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2026-02-11 20:53:00 +0100

    Updating deploy.bat file

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,22 @@
-cd utils
-call mvn clean package install
+@call cd ..
 
-cd ..
-call mvn clean package install
+@rem Downloading dependencies
+@if not exist utils (
+	@echo Cloning git repo for project utils
+    @call git clone --branch 2.0-SNAPSHOT --single-branch https://github.com/Pierre-Emmanuel41/utils.git
+	
+	@call cd utils
+) else ( 
+	@call cd utils
+	
+	@echo Pulling latest changes for project utils
+	@call git pull
+)
 
-pause
+@rem Building dependencies
+@echo Building project utils
+@call mvn clean package install
+
+@echo Building project sound
+@call cd ../sound
+@call mvn clean package install
